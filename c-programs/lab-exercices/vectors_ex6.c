941932bd26bbc9b4a95534fb2571925043e94308
Luiz Baldi <baldilp@gmail.com>
2016-11-16 17:52:41 -0200

    fixing vector exercise in c

--- c-programs/lab-exercices/vectors_ex6.c
+++ c-programs/lab-exercices/vectors_ex6.c
@@ -4,12 +4,12 @@
 int getRandomNumber(int startNumber, int endNumber) {
 	int randomNumber;
 
-	randomNumber = rand() % 10;
+	randomNumber = rand() % 100;
 
 	if (randomNumber >= startNumber && randomNumber <= endNumber) {
-		getRandomNumber(startNumber, endNumber);
-	} else {
 		return randomNumber;
+	} else {
+		getRandomNumber(startNumber, endNumber);
 	}
 }
 
@@ -17,15 +17,15 @@ int main() {
 	int vet[4][4];
     int row, col;
     
-	for(row = 0; col < 4; row++) {
+	for(row = 0; row < 4; row++) {
 		for(col = 0; col < 4; col++) {
 			vet[row][col] = getRandomNumber(15, 50);
 		}
 	}
 
-	for(row = 0; col < 4; row++) {
+	for(row = 0; row < 4; row++) {
 		for(col = 0; col < 4; col++) {
-			printf("vet[%d][%d]\n", row, col);
+			printf("vet[%d][%d] = %d\n", row, col, vet[row][col]);
 		}
 	}
 
